5363a4903ad4188e1dc590024bbf47dcac586c6a
dhanushsensehawk <123460947+dhanushsensehawk@users.noreply.github.com>
2023-12-07 11:49:27 +0530

    Packer Installation Script

---
+++ install_scripts/packer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add HashiCorp GPG key
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+# Add HashiCorp repository
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+# Update package lists and install Packer
+sudo apt-get update && sudo apt-get install packer

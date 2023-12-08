b96a029392a1db93d24cddad88f51d3c2d1b406b
dhanushsensehawk <123460947+dhanushsensehawk@users.noreply.github.com>
2023-12-08 16:29:06 +0530

    Install traefik to the existed instance

---
+++ terraform_files/traefik_instance.tf
@@ -0,0 +1,13 @@
+data "aws_instance" "client_terraform" {
+  instance_id = "i-0e60b5b85eef7285f"
+}
+
+resource "null_resource" "install_packer" {
+  triggers = {
+    instance_id = data.aws_instance.client_terraform.id
+  }
+
+  provisioner "local-exec" {
+    command = "ssh -i /home/ubuntu/keys/To_Be_Deleted_Singapore.pem ${data.aws_instance.client_terraform.private_ip} 'bash -s' < /home/ubuntu/install_scripts/traefik.sh"
+  }
+}
